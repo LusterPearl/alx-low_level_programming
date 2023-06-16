0e5223ee539303250bc724305af9076856d28a78
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-16 14:20:32 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
-			if (num1 == 90 && num2 == 99)
+			if (num1 == 98 && num2 == 99)
 				continue;
 
 			putchar(',');
