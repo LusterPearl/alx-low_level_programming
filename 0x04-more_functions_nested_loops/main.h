bde11d35a921af0a8cd8d42bf02b5331236dc49e
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-23 04:46:46 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,23 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
+ */
+int main(void)
+
 /**
  * _isupper - uppercase letters
  * @c: character to check
