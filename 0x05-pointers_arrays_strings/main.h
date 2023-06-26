3aacc378d51020bfa9f0dde986bb944e80e449be
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-26 08:15:55 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -49,7 +49,14 @@ void print_rev(char *s);
  */
 void rev_string(char *s);
 
+/**
+ * puts2 -  function that prints every other character of a string, 
+ * starting with the first character, followed by a new line
+ * @str: input
+ * Return: print
+ */
 void puts2(char *str);
+
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
