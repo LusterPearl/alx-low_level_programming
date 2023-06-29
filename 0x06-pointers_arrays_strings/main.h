170db790d4f222311682784d85cf11e0e46788b9
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-29 09:38:55 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,19 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
 char *_strcat(char *dest, char *src);
+
+
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
