51c571902696eac09969eec691bf667da0736916
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-29 10:09:10 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -32,7 +32,15 @@ char *_strncat(char *dest, char *src, int n);
  */
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
+ */
 int _strcmp(char *s1, char *s2);
+
+
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
