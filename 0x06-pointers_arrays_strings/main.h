17ebd2aeb1b0bf88c852cbaec1378cb9c1b61e5d
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-29 10:59:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -55,6 +55,11 @@ void reverse_array(int *a, int n);
  */
 char *string_toupper(char *);
 
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ * Return: A pointer to the changed string.
+ */
 char *cap_string(char *);
 
 
