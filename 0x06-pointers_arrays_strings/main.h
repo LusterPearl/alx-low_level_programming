3b0c07b502c4281164861debaa996b1e4a5b22f0
LusterPearl <salamioluwatosin90@gmail.com>
2023-06-29 10:17:41 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -40,8 +40,14 @@ char *_strncpy(char *dest, char *src, int n);
  */
 int _strcmp(char *s1, char *s2);
 
-
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Return: void
+ */
 void reverse_array(int *a, int n);
+
 char *string_toupper(char *);
 char *cap_string(char *);
 
