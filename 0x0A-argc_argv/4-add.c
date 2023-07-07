98130c1edd2377e874e46392526f325014b50cfd
LusterPearl <salamioluwatosin90@gmail.com>
2023-07-07 10:13:40 -0700

    100-change.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 	{
 		if (check_num(argv[count]))
 		{
-			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			str_to_int = atoi (argv[count]); /*ATOI --> convert string to int*/
 					sum += str_to_int;
 		}
 		/*Condition if one of the number contains symbols that are not digits*/
