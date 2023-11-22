cf7d3c56318b150d2b5a6df63c11622853798270
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-22 07:28:57 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid.
+
+    Args:
+    grid: list of list of integers representing the island
+
+    Returns:
+    Perimeter of the island
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
