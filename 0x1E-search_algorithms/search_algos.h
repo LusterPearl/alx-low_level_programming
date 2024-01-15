1707483691e7c0f0420c74889ed7c3e844db36fe
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-15 05:52:24 -0800

    firstpart

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif/*MAIN_H*/
