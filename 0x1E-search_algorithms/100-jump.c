cbf8dbafc9c92009a453d26429d9986526a72d5e
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-17 13:14:44 -0800

    wait

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -23,11 +23,9 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev += step;
 		if (prev < size)
-			printf("Value checked array[%lu] = [%d]\n", prev,
-				array[prev]);
-		else
 			break;
 	}
+
 	printf("Value found between indexes [%lu] and [%lu]\n",
 		prev - step, prev);
 	for (i = prev - step; i < prev && i < size; i++)
